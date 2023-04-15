12e078a3222f4ef9dfe2619090ca9e9ba51b0fc0
Gaston Bundio <gasty_desarandi@hotmail.com>
2023-04-15 09:43:16 -0300

    cambios

--- prueba.php
+++ prueba.php
@@ -4,4 +4,11 @@ function concatenar ($cadena1, $cadena2){
 }
 $resultado = concatenar("Arsenal "," De Sarandi ");
 echo $resultado;
+
+function saludar($nombre,$apellido=""){
+return "Hola $nombre $apellido";
+}
+$saludo=saludar("Gaston", "Bundio");
+echo $saludo;
+
 ?>
